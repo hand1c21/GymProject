2c1fcf07b1dcb08c2e826f807410dde8f3163a4d
מילר תהילה < @mbjcomp.local>
2024-02-09 11:14:03 +0200

    adding Icrud

---
+++ MyProject/MyProject/Controllers/TrainersController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TrainersController : ControllerBase
+    {
+    }
+}
